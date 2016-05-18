3c85c505fa7aa17da8131ad3f656fde9dc60b441
Preston <pshowell23@gmail.com>
2016-05-18 16:19:46 -0400

    Glyphicons successfully added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 jQuery(document).ready(function($){
-	$('.secondButtons > p').append('<i class="glyphicon glyphicon-question-sign"></i>');
+	$('.secondButtons p:first-child').append('<br/><i class="glyphicon glyphicon-question-sign"></i>');
+	$('.secondButtons p:nth-child(2)').append('<br/><i class="glyphicon glyphicon-book"></i>');
+	$('.secondButtons p:nth-child(3)').append('<br/><i class="glyphicon glyphicon-education"></i>');
 });
