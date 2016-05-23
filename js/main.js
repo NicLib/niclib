7de6e7922de7e43497783702d67eced4517869c4
Preston <pshowell23@gmail.com>
2016-05-23 16:05:28 -0400

    Custom script for Library H3lp

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function($){
-	$('.secondButtons p:first-child').append('<br/><i class="glyphicon glyphicon-question-sign"></i>');
+	$('.secondButtons p:first-child').append('<br/><i class="glyphicon glyphicon-question-sign"></i>').click(function(){
+		//Custom script for opening a window for Library H3lp
+		window.open('https://libraryh3lp.com/chat/nicref@chat.libraryh3lp.com?skin=20466', 'AskUs', 'resizable=1,width=500,height=500')
+	});
 	$('.secondButtons p:nth-child(2)').append('<br/><i class="glyphicon glyphicon-book"></i>');
 	$('.secondButtons p:nth-child(3)').append('<br/><i class="glyphicon glyphicon-education"></i>');
 });
