22776698c81f6f602e7f253038afe044ac53bcd8
Preston <pshowell23@gmail.com>
2016-05-25 16:25:25 -0400

    Begin work on secondary menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 jQuery(document).ready(function($){
+	$('.NicLib-Head a').click(function(event){
+		event.preventDefault();
+	});
 	$('.secondButtons p:first-child').append('<br/><i class="glyphicon glyphicon-question-sign"></i>').click(function(){
 		//Custom script for opening a window for Library H3lp
 		window.open('https://libraryh3lp.com/chat/nicref@chat.libraryh3lp.com?skin=26324', 'AskUs', 'resizable=1,width=500,height=500')
 	});
 	$('.secondButtons p:nth-child(2)').append('<br/><i class="glyphicon glyphicon-book"></i>');
 	$('.secondButtons p:nth-child(3)').append('<br/><i class="glyphicon glyphicon-education"></i>');
-});
+});	
