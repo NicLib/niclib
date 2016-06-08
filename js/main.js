5b804f33b4f38ae7e681b53250ab6711f6c7a35b
Preston <pshowell23@gmail.com>
2016-06-08 11:36:42 -0400

    Working on Menu animation

--- js/main.js
+++ js/main.js
@@ -1,41 +1,50 @@
 var activeMenu = [];
 
 jQuery(document).ready(function($){
-	$('.NicLib-Head a').click(function(event){
-		event.preventDefault();
-		var menus = ['.servicesMenu', '.collectionsMenu', '.aboutMenu', '.helpMenu'];
-		var menuSelected = '.' + $(this).text().toLowerCase() + 'Menu';
-		var activateMenu = function(selection){
-			activeMenu = [];
-			activeMenu.push(selection);
-		};
-		
-		//Creates 'Hidden Menu' to open
-		if($(this).parents('.NicLib-Head').next().css('display') == 'none'){
-			$(this).parents('.NicLib-Head').next().slideDown();
-		};
-		
-		//Causes 'Menu Links' to appear one at a time
-		for(var menu in menus){
-			$(menus[menu]).css('display', 'none');
-		};
-		
-		$(menuSelected).css('display','initial');
-		
-		if($('activeMenu[0]').attr('height') != $(menuSelected).attr('height')){
-			$('.hiddenMenu').animate('height');
-		}
-		
-		activateMenu(menuSelected);
-	});
-	//Causes 'Hidden Menu' to close
-	$('.hiddenMenu i').click(function(){
-		$(this).parents('.hiddenMenu').slideUp();
-	});
+	//Adds Glyphicons to 'secondButtons'
 	$('.secondButtons p:first-child').append('<br/><i class="glyphicon glyphicon-question-sign"></i>').click(function(){
 		//Custom script for opening a window for Library H3lp
 		window.open('https://libraryh3lp.com/chat/nicref@chat.libraryh3lp.com?skin=26324', 'AskUs', 'resizable=1,width=500,height=500')
 	});
 	$('.secondButtons p:nth-child(2)').append('<br/><i class="glyphicon glyphicon-book"></i>');
 	$('.secondButtons p:nth-child(3)').append('<br/><i class="glyphicon glyphicon-education"></i>');
+	
+	//Navigation actions
+	$('.NicLib-Head a').click(function(event){
+		event.preventDefault();
+		var hiddenMenu = $(this).parents('.NicLib-Head').next();
+		var allMenus = ['.servicesMenu', '.collectionsMenu', '.aboutMenu', '.helpMenu'];
+		var menuSelected = '.' + $(this).text().toLowerCase() + 'Menu';
+		var activateMenu = function(selection){
+			activeMenu = [];
+			activeMenu.push(selection);
+		};
+		
+		//Causes dynamic menu sizes to change smoothly
+		if(activeMenu.length == 0){
+			$(hiddenMenu).slideDown();
+		} else if ($(activeMenu[0]).attr('height') != $(menuSelected).attr('height')){
+			//This isn't working the way I want
+				//Different jQuery function?
+				//Something in the wrong order?
+				//Not reading the DOM as anticipated?
+			$(hiddenMenu).animate({'height':'100%'}, 'slow');
+		};
+		
+		//Causes 'Menu Links' to appear one at a time
+		for(var menu in allMenus){
+			$(allMenus[menu]).css('display', 'none');
+		};
+		$(menuSelected).css('display','initial');
+		
+		//Bug Testing
+		console.log(activeMenu);
+		activateMenu($(menuSelected));
+		console.log(activeMenu);
+	});
+	
+	//Causes 'hiddenMenu' to close
+	$('.hiddenMenu i').click(function(){
+		$(this).parents('.hiddenMenu').slideUp();
+	});
 });	
