7d4ffe021ef6b76907366cff6e1342b3c412181f
Preston Howell <pshowell23@gmail.com>
2016-10-13 13:04:56 -0400

    Remove search bar from iframe

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ jQuery(document).ready(function($){
 		//Function found from StackOverflow user Popnoodles
 		//link "http://jsfiddle.net/zbB3Q/"
 		dynamicHeight();
-	}).mouseenter(function(){
+	})/*.mouseenter(function(){
 		//Below code is the same as the 'click' function
 		var menu = $(this);
 		var hiddenMenu = menu.parents('.NicLib-Head').next();
@@ -75,7 +75,7 @@ jQuery(document).ready(function($){
 		};
 		$(menuSelected).css('display','block');
 		dynamicHeight();
-	});
+	})*/;
 	
 	//Causes 'hiddenMenu' to close on mouse out
 	$('.NicLib-Head').parents('.row').mouseleave(function(){
@@ -88,5 +88,53 @@ jQuery(document).ready(function($){
 	//Causes 'hiddenMenu' to close
 	$('.hiddenMenu i').click(function(){
 		$(this).parents('.hiddenMenu').slideUp();
+	});
+	
+	//Function for Tabs in Searchbox
+	$('.tabs .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
+ 
+		// Show/Hide Tabs
+		$('.tabs ' + currentAttrValue).show().siblings().hide();
+ 
+		// Change/remove current tab to active
+		//jQuery(this).closest('h2').addClass('active').siblings().removeClass('active');
+		$('.tabs .tab-links a.selected').removeClass('selected');
+		$(this).addClass('selected');
+ 
+		e.preventDefault();
+	});
+			
+	//retain search term input when switching tabs
+	$('#query1').change(function(){
+		$('.searchbox').val($('#query1').val());
+	});
+	$('#query2').change(function(){
+		$('.searchbox').val($('#query2').val());
+	});
+	$('#query3').change(function(){
+		$('.searchbox').val($('#query3').val());
+	});
+	$('#query4').change(function(){
+		$('.searchbox').val($('#query4').val());
+	});
+	
+	//Found at https://www.sitepoint.com/community/t/adding-removing-classes-on-resize-jquery/191805
+	$(window).on("load", function(){
+		var viewportWidth = $(window).width();
+		if(viewportWidth < 200){
+			$('.tab-links h2').removeClass();
+		};
+	});
+	
+	$('.windowForm').submit(function(e){
+		// specify a unique target name
+		var target = 'windowFormTarget';
+		// open a new window and name it
+		window.open('', target, 'width=500,height=300');
+		// set the target of the form to be
+		// the window name
+		this.setAttribute('target', target);
+		// allow the form to be submitted normally
 	});
 });	
